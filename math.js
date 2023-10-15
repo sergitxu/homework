a1ec1cdf3b22a85a8790f29686d8a8e3bd196d45
Sergio Ortiz <sergio.ortiz@visma.com>
2023-10-15 22:31:30 +0200

    añadir control de puntos y vidas

--- math.js
+++ math.js
@@ -1,35 +1,92 @@
-var resultado;
-function crearOperaciones(numOperaciones) {
-    var ul = document.getElementById('operaciones');
-    for (var i = 1; i <= numOperaciones; i++) {
-        ul.innerHTML += "\n        <li>\n            <form id=\"calculo".concat(i, "\">\n                <span id=\"cifra").concat(i, "a\" class=\"cifra\">").concat(randomNumber(10), "</span>\n                <span id=\"operacion").concat(i, "\" class=\"operacion\">+</span>\n                <span id=\"cifra").concat(i, "b\" class=\"cifra\">").concat(randomNumber(10), "</span>\n                <span class=\"cifra\"> = </span>\n                <input type=\"number\" id=\"respuesta").concat(i, "\" name=\"respuesta1\" class=\"respuesta\">\n                <button type=\"button\" id=\"calcular").concat(i, "\" onclick=\"calcular(").concat(i, ")\">Comprobar</button>\n            </form>\n        </li>\n        ");
+var puntos = 0;
+var VIDAS_INICIALES = 5;
+var vidas = VIDAS_INICIALES;
+actualizarPuntos();
+actualizarVidas();
+function actualizarPuntos() {
+    document.getElementById('puntos').innerText = puntos.toString();
+}
+function gameOver() {
+    alert('GAME OVER');
+    mostrar('empezar');
+}
+;
+function actualizarVidas() {
+    document.getElementById('vidas').innerHTML = '';
+    for (var i = 1; i <= vidas; i++) {
+        document.getElementById('vidas').innerHTML += "&#9733";
+    }
+    for (var i = 1; i <= VIDAS_INICIALES - vidas; i++) {
+        document.getElementById('vidas').innerHTML += "<span class=\"corazon-vacio\">&#9734</span>";
+    }
+    if (vidas === 0) {
+        gameOver();
     }
 }
-function calcular(numOperacion) {
+function esconder(paraEsconder) {
+    document.getElementById("".concat(paraEsconder)).style.display = 'none';
+}
+function mostrar(paraEsconder) {
+    document.getElementById("".concat(paraEsconder)).style.display = 'block';
+}
+function crearOperacion() {
+    vidas = VIDAS_INICIALES;
+    puntos = 0;
+    actualizarVidas();
+    actualizarPuntos();
+    var ul = document.getElementById('operaciones');
+    var operacion;
+    var operacionAzar = randomNumber(2);
+    var valor_a = randomNumber(11);
+    var valor_b;
+    if (operacionAzar === 0) {
+        operacion = "+";
+        valor_b = randomNumber(11);
+    }
+    else {
+        operacion = "-";
+        valor_b = randomNumber(valor_a);
+        console.log(valor_a);
+        console.log(valor_b);
+        console.log(operacion);
+    }
+    ul.innerHTML = "\n        <li>\n            <form id=\"calculo\">\n                <span id=\"cifra-a\" class=\"cifra\">".concat(valor_a, "</span>\n                <span id=\"operacion\" class=\"operacion\">").concat(operacion, "</span>\n                <span id=\"cifra-b\" class=\"cifra\">").concat(valor_b, "</span>\n                <span class=\"cifra\"> = </span>\n                <input type=\"number\" id=\"respuesta\" name=\"respuesta\" class=\"respuesta\">\n                <button type=\"button\" id=\"calcularBoton\" onclick=\"calcular()\" class=\"boton-calcular\">&#9166;</button>\n            </form>\n        </li>\n        ");
+}
+function calcular() {
     var _a;
-    var a = document.getElementById("cifra".concat(numOperacion, "a")).innerText;
-    var b = document.getElementById("cifra".concat(numOperacion, "b")).innerText;
-    var operacion = (_a = document.getElementById("operacion".concat(numOperacion))) === null || _a === void 0 ? void 0 : _a.innerText;
-    var botonCalcular = document.getElementById("calcular".concat(numOperacion));
-    var respuesta = document.getElementById("respuesta".concat(numOperacion)).value;
+    var a = document.getElementById("cifra-a").innerText;
+    var b = document.getElementById("cifra-b").innerText;
+    var operacion = (_a = document.getElementById("operacion")) === null || _a === void 0 ? void 0 : _a.innerText;
+    var respuesta = document.getElementById("respuesta").value;
+    var resultado = 0;
     a = parseInt(a);
     b = parseInt(b);
     respuesta = parseInt(respuesta);
     if (operacion === '+') {
-        this.resultado = a + b;
+        resultado = a + b;
     }
-    // else if(operacion === '-') {
-    //     resultado = a - b;
-    // }
+    else if (operacion === '-') {
+        resultado = a - b;
+    }
     else
         (console.error('No sé que operación es esa.'));
-    if (respuesta === this.resultado) {
-        alert('BRAVO');
-        botonCalcular.disabled = true;
+    if (respuesta === resultado) {
+        acertar();
     }
     else {
-        alert('MAL, prueba otra vez.');
+        fallar();
     }
+}
+function acertar() {
+    alert('BRAVO');
+    puntos++;
+    actualizarPuntos();
+    crearOperacion();
+}
+function fallar() {
+    alert('MAL, prueba otra vez.');
+    vidas--;
+    actualizarVidas();
 }
 function randomNumber(lessThan) {
     return Math.floor(Math.random() * lessThan);
--- math.ts
+++ math.ts
@@ -1,51 +1,115 @@
-let resultado:number;
+let puntos:number = 0;
+let VIDAS_INICIALES:number = 5;
+let vidas:number = VIDAS_INICIALES;
 
-function crearOperaciones(numOperaciones) {
+actualizarPuntos();
+actualizarVidas();
+
+function actualizarPuntos() {
+    document.getElementById('puntos')!.innerText = puntos.toString();
+}
+function gameOver() {
+    alert('GAME OVER');
+    mostrar('empezar');
+};
+
+function actualizarVidas() {
+    
+    document.getElementById('vidas')!.innerHTML = '';
+
+    for(let i = 1; i<= vidas; i++) {
+        document.getElementById('vidas')!.innerHTML += `&#9733`;
+    }
+    for(let i = 1; i<= VIDAS_INICIALES - vidas; i++) {
+        document.getElementById('vidas')!.innerHTML += `<span class="corazon-vacio">&#9734</span>`;
+    }
+
+    if (vidas === 0) {
+        gameOver();
+    }
+    
+}
+
+function esconder(paraEsconder) {
+    document.getElementById(`${paraEsconder}`)!.style.display = 'none';
+}
+function mostrar(paraEsconder) {
+    document.getElementById(`${paraEsconder}`)!.style.display = 'block';
+}
+
+function crearOperacion() {
+    vidas = VIDAS_INICIALES;
+    puntos = 0;
+    
+    actualizarVidas();
+    actualizarPuntos();
+
     var ul = document.getElementById('operaciones');
+    let operacion:string;
+    let operacionAzar:number = randomNumber(2);
+    let valor_a:number = randomNumber(11);
+    let valor_b:number;
 
-    for (let i = 1; i<=numOperaciones; i++) {
-    ul!.innerHTML += `
+    if (operacionAzar === 0){
+        operacion = "+";
+        valor_b = randomNumber(11);
+    } else {
+        operacion = "-";
+        valor_b = randomNumber(valor_a);
+    }
+
+    ul!.innerHTML = `
         <li>
-            <form id="calculo${i}">
-                <span id="cifra${i}a" class="cifra">${randomNumber(10)}</span>
-                <span id="operacion${i}" class="operacion">+</span>
-                <span id="cifra${i}b" class="cifra">${randomNumber(10)}</span>
+            <form id="calculo">
+                <span id="cifra-a" class="cifra">${valor_a}</span>
+                <span id="operacion" class="operacion">${operacion}</span>
+                <span id="cifra-b" class="cifra">${valor_b}</span>
                 <span class="cifra"> = </span>
-                <input type="number" id="respuesta${i}" name="respuesta1" class="respuesta">
-                <button type="button" id="calcular${i}" onclick="calcular(${i})">Comprobar</button>
+                <input type="number" id="respuesta" name="respuesta" class="respuesta">
+                <button type="button" id="calcularBoton" onclick="calcular()" class="boton-calcular">&#9166;</button>
             </form>
         </li>
         `;
-    }
 }
 
-function calcular(numOperacion) {
-    let a: string|number = document.getElementById(`cifra${numOperacion}a`)!.innerText;
-    let b: string|number = document.getElementById(`cifra${numOperacion}b`)!.innerText;
-    let operacion = document.getElementById(`operacion${numOperacion}`)?.innerText;
-    let botonCalcular = (<HTMLInputElement>document.getElementById(`calcular${numOperacion}`))!;
-    let respuesta:string|number = (<HTMLInputElement>document.getElementById(`respuesta${numOperacion}`)).value;
+function calcular() {
+    let a: string|number = document.getElementById(`cifra-a`)!.innerText;
+    let b: string|number = document.getElementById(`cifra-b`)!.innerText;
+    let operacion = document.getElementById(`operacion`)?.innerText;
+    let respuesta:string|number = (<HTMLInputElement>document.getElementById(`respuesta`)).value;
+    let resultado:number = 0;
 
     a = parseInt(a);
     b = parseInt(b);
     respuesta = parseInt(respuesta);
 
     if (operacion === '+') {
-        this.resultado = a + b;
+        resultado = a + b;
     }
-    // else if(operacion === '-') {
-    //     resultado = a - b;
-    // }
+    else if(operacion === '-') {
+        resultado = a - b;
+    }
     else (
         console.error('No sé que operación es esa.')
     )
     
-    if (respuesta === this.resultado) {
-        alert('BRAVO');
-        botonCalcular.disabled = true;
+    if (respuesta === resultado) {
+        acertar();
     } else {
-        alert('MAL, prueba otra vez.')
+        fallar();
     }
+}
+
+function acertar() {
+    alert('BRAVO');
+    puntos++;
+    actualizarPuntos();
+    crearOperacion();
+}
+function fallar() {
+    alert('MAL, prueba otra vez.');
+    vidas--;
+    actualizarVidas();
 }
 
 function randomNumber(lessThan) {    
