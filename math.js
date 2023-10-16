02c7f819c235d1bb37ece4c174bb4aa3adc4cc00
Sergio Ortiz <sergio.ortiz@visma.com>
2023-10-16 11:48:03 +0200

    añadir class y resultaod correcto

--- math.js
+++ math.js
@@ -1,25 +1,27 @@
-var MyHomework = {
-    puntos: 0,
-    VIDAS_INICIALES: 5,
-    vidas: this.VIDAS_INICIALES,
-    formulario: document.getElementById('calculo'),
-    // TODO añadir record personal usando storage
-    init: function () {
-        var _a;
+var Homework = /** @class */ (function () {
+    function Homework() {
+        var _this = this;
+        this.puntos = 0;
+        this.VIDAS_INICIALES = 5;
+        this.vidas = this.VIDAS_INICIALES;
+        this.resultado = 0;
+        this.formulario = document.getElementById('calculo');
         this.actualizarPuntos();
         this.actualizarVidas();
+        // TODO añadir record personal usando storage
         // Manejo de la tecla enter
-        (_a = this.formulario) === null || _a === void 0 ? void 0 : _a.addEventListener('keypress', function (e) {
+        var formulario = document.getElementById('calculo');
+        formulario === null || formulario === void 0 ? void 0 : formulario.addEventListener('keypress', function (e) {
             if (e.key === 'Enter') {
                 e.preventDefault();
-                this.calcular();
+                _this.calcular();
             }
         });
-    },
-    actualizarPuntos: function () {
+    }
+    Homework.prototype.actualizarPuntos = function () {
         document.getElementById('puntos').innerText = this.puntos.toString();
-    },
-    actualizarVidas: function () {
+    };
+    Homework.prototype.actualizarVidas = function () {
         document.getElementById('vidas').innerHTML = '';
         for (var i = 1; i <= this.vidas; i++) {
             document.getElementById('vidas').innerHTML += '&#9733';
@@ -30,14 +32,14 @@ var MyHomework = {
         if (this.vidas === 0) {
             this.gameOver();
         }
-    },
-    resetearVidasPuntos: function () {
+    };
+    Homework.prototype.resetearVidasPuntos = function () {
         this.vidas = this.VIDAS_INICIALES;
         this.puntos = 0;
         this.actualizarVidas();
         this.actualizarPuntos();
-    },
-    crearOperacion: function () {
+    };
+    Homework.prototype.crearOperacion = function () {
         var _a;
         var operacion;
         var operacionAzar = this.randomNumber(2);
@@ -51,68 +53,70 @@ var MyHomework = {
             operacion = "-";
             valor_b = this.randomNumber(valor_a);
         }
-        this.formulario.innerHTML = "\n            <span id=\"cifra-a\" class=\"cifra\">".concat(valor_a, "</span>\n            <span id=\"operacion\" class=\"operacion\">").concat(operacion, "</span>\n            <span id=\"cifra-b\" class=\"cifra\">").concat(valor_b, "</span>\n            <span class=\"cifra\"> = </span>\n            <input type=\"number\" id=\"respuesta\" name=\"respuesta\" class=\"respuesta\" oninput=\"quitarError()\">\n            <button type=\"button\" id=\"calcularBoton\" onclick=\"calcular()\" class=\"boton-calcular\">&#9166;</button>\n        ");
+        this.formulario.innerHTML = "\n            <span id=\"cifra-a\" class=\"cifra\">".concat(valor_a, "</span>\n            <span id=\"operacion\" class=\"operacion\">").concat(operacion, "</span>\n            <span id=\"cifra-b\" class=\"cifra\">").concat(valor_b, "</span>\n            <span class=\"cifra\"> = </span>\n            <input type=\"number\" id=\"respuesta\" name=\"respuesta\" class=\"respuesta\" oninput=\"homeWork.quitarError()\">\n            <button type=\"button\" id=\"calcularBoton\" onclick=\"calcular()\" class=\"boton-calcular\">&#9166;</button>\n        ");
         (_a = document.getElementById("respuesta")) === null || _a === void 0 ? void 0 : _a.focus();
-    },
-    calcular: function () {
+    };
+    Homework.prototype.calcular = function () {
         var _a;
         if (this.vidas > 0) {
             var a = document.getElementById("cifra-a").innerText;
             var b = document.getElementById("cifra-b").innerText;
             var operacion = (_a = document.getElementById("operacion")) === null || _a === void 0 ? void 0 : _a.innerText;
             var respuesta = document.getElementById("respuesta").value;
-            var resultado = 0;
             a = parseInt(a);
             b = parseInt(b);
             respuesta = parseInt(respuesta);
             if (operacion === '+') {
-                resultado = a + b;
+                this.resultado = a + b;
             }
             else if (operacion === '-') {
-                resultado = a - b;
+                this.resultado = a - b;
             }
             else
                 (console.error('No sé que operación es esa.'));
-            if (respuesta === resultado) {
+            if (respuesta === this.resultado) {
                 this.acertar();
             }
             else {
                 this.fallar();
             }
         }
-    },
-    acertar: function () {
+    };
+    Homework.prototype.acertar = function () {
         alert('BRAVO');
         this.puntos++;
         this.actualizarPuntos();
         this.crearOperacion();
-    },
-    fallar: function () {
+    };
+    Homework.prototype.fallar = function () {
         var _a;
         (_a = document.getElementById("respuesta")) === null || _a === void 0 ? void 0 : _a.classList.add('error');
         alert('MAL, prueba otra vez.');
         this.vidas--;
         this.actualizarVidas();
-    },
-    gameOver: function () {
+    };
+    Homework.prototype.gameOver = function () {
         alert('GAME OVER');
         document.getElementById("calcularBoton").disabled = true;
         this.mostrar('empezar');
-        this.formulario.innerHTML = "<p>La respuesta correcta era: ".concat(this.resultado, "</p>");
-        // TODO añadir resultado correcto
-    },
-    quitarError: function () {
+        // TODO: Add correct result
+        this.formulario.innerHTML += "<p>La respuesta correcta es <span class=\"resultado-number\">".concat(this.resultado, "</span>.</p>");
+    };
+    Homework.prototype.quitarError = function () {
         var _a;
         (_a = document.getElementById("respuesta")) === null || _a === void 0 ? void 0 : _a.classList.remove('error');
-    },
-    randomNumber: function (lessThan) {
+    };
+    Homework.prototype.randomNumber = function (lessThan) {
         return Math.floor(Math.random() * lessThan);
-    },
-    esconder: function (paraEsconder) {
-        document.getElementById("".concat(paraEsconder)).style.display = 'none';
-    },
-    mostrar: function (paraMostrar) {
-        document.getElementById("".concat(paraMostrar)).style.display = 'block';
-    }
-};
-this.init();
+    };
+    Homework.prototype.esconder = function (paraEsconder) {
+        document.getElementById(paraEsconder).style.display = 'none';
+    };
+    Homework.prototype.mostrar = function (paraMostrar) {
+        document.getElementById(paraMostrar).style.display = 'block';
+    };
+    return Homework;
+}());
+;
+// Create an instance of the MathGame class
+var homeWork = new Homework();
--- math.ts
+++ math.ts
@@ -1,137 +1,148 @@
-let puntos: number = 0;
-let VIDAS_INICIALES: number = 5;
-let vidas: number = VIDAS_INICIALES;
+class Homework {
+    puntos: number = 0;
+    VIDAS_INICIALES: number = 5;
+    vidas: number = this.VIDAS_INICIALES;
+    resultado: number = 0;
+    formulario = document.getElementById('calculo');
 
-const formulario = document.getElementById('calculo');
+    constructor() {
+        this.actualizarPuntos();
+        this.actualizarVidas();
 
-actualizarPuntos();
-actualizarVidas();
+        // TODO añadir record personal usando storage
 
-// TODO añadir record personal usando storage
-
-// Manejo de la tecla enter
-formulario?.addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-        e.preventDefault();
-        calcular();
-    }
-});
-
-function actualizarPuntos() {
-    document.getElementById('puntos')!.innerText = puntos.toString();
-}
-
-function actualizarVidas() {
-
-    document.getElementById('vidas')!.innerHTML = '';
-
-    for (let i = 1; i <= vidas; i++) {
-        document.getElementById('vidas')!.innerHTML += '&#9733';
-    }
-    for (let i = 1; i <= VIDAS_INICIALES - vidas; i++) {
-        document.getElementById('vidas')!.innerHTML += '&#9734';
+        // Manejo de la tecla enter
+        const formulario = document.getElementById('calculo');
+        formulario?.addEventListener('keypress', (e) => {
+            if (e.key === 'Enter') {
+                e.preventDefault();
+                this.calcular();
+            }
+        });
     }
 
-    if (vidas === 0) {
-        gameOver();
-    }
-}
 
-function resetearVidasPuntos() {
-    vidas = VIDAS_INICIALES;
-    puntos = 0;
-    actualizarVidas();
-    actualizarPuntos();
-}
-
-function crearOperacion() {
-    let operacion: string;
-    let operacionAzar: number = randomNumber(2);
-    let valor_a: number = randomNumber(11);
-    let valor_b: number;
-
-    if (operacionAzar === 0) {
-        operacion = "+";
-        valor_b = randomNumber(11);
-    } else {
-        operacion = "-";
-        valor_b = randomNumber(valor_a);
+    actualizarPuntos() {
+        document.getElementById('puntos')!.innerText = this.puntos.toString();
     }
 
-    formulario!.innerHTML = `
+    actualizarVidas() {
+
+        document.getElementById('vidas')!.innerHTML = '';
+
+        for (let i = 1; i <= this.vidas; i++) {
+            document.getElementById('vidas')!.innerHTML += '&#9733';
+        }
+        for (let i = 1; i <= this.VIDAS_INICIALES - this.vidas; i++) {
+            document.getElementById('vidas')!.innerHTML += '&#9734';
+        }
+
+        if (this.vidas === 0) {
+            this.gameOver();
+        }
+    }
+
+    resetearVidasPuntos() {
+        this.vidas = this.VIDAS_INICIALES;
+        this.puntos = 0;
+        this.actualizarVidas();
+        this.actualizarPuntos();
+    }
+
+    crearOperacion() {
+        let operacion: string;
+        let operacionAzar: number = this.randomNumber(2);
+        let valor_a: number = this.randomNumber(11);
+        let valor_b: number;
+
+        if (operacionAzar === 0) {
+            operacion = "+";
+            valor_b = this.randomNumber(11);
+        } else {
+            operacion = "-";
+            valor_b = this.randomNumber(valor_a);
+        }
+
+        this.formulario!.innerHTML = `
             <span id="cifra-a" class="cifra">${valor_a}</span>
             <span id="operacion" class="operacion">${operacion}</span>
             <span id="cifra-b" class="cifra">${valor_b}</span>
             <span class="cifra"> = </span>
-            <input type="number" id="respuesta" name="respuesta" class="respuesta" oninput="quitarError()">
+            <input type="number" id="respuesta" name="respuesta" class="respuesta" oninput="homeWork.quitarError()">
             <button type="button" id="calcularBoton" onclick="calcular()" class="boton-calcular">&#9166;</button>
         `;
-    document.getElementById(`respuesta`)?.focus();
-}
 
-function calcular() {
-    if (vidas > 0) {
-        let a: string | number = document.getElementById(`cifra-a`)!.innerText;
-        let b: string | number = document.getElementById(`cifra-b`)!.innerText;
-        let operacion = document.getElementById(`operacion`)?.innerText;
-        let respuesta: string | number = (<HTMLInputElement>document.getElementById(`respuesta`)).value;
-        let resultado: number = 0;
+        document.getElementById(`respuesta`)?.focus();
+    }
 
-        a = parseInt(a);
-        b = parseInt(b);
-        respuesta = parseInt(respuesta);
+    calcular() {
+        if (this.vidas > 0) {
+            let a: string | number = document.getElementById(`cifra-a`)!.innerText;
+            let b: string | number = document.getElementById(`cifra-b`)!.innerText;
+            let operacion = document.getElementById(`operacion`)?.innerText;
+            let respuesta: string | number = (<HTMLInputElement>document.getElementById(`respuesta`)).value;
 
-        if (operacion === '+') {
-            resultado = a + b;
-        }
-        else if (operacion === '-') {
-            resultado = a - b;
-        }
-        else (
-            console.error('No sé que operación es esa.')
-        )
+            a = parseInt(a);
+            b = parseInt(b);
+            respuesta = parseInt(respuesta);
 
-        if (respuesta === resultado) {
-            acertar();
-        } else {
-            fallar();
+            if (operacion === '+') {
+                this.resultado = a + b;
+            }
+            else if (operacion === '-') {
+                this.resultado = a - b;
+            }
+            else (
+                console.error('No sé que operación es esa.')
+            )
+
+            if (respuesta === this.resultado) {
+                this.acertar();
+            } else {
+                this.fallar();
+            }
         }
     }
-}
 
-function acertar() {
-    alert('BRAVO');
-    puntos++;
-    actualizarPuntos();
-    crearOperacion();
-}
+    acertar() {
+        alert('BRAVO');
+        this.puntos++;
+        this.actualizarPuntos();
+        this.crearOperacion();
+    }
 
-function fallar() {
-    document.getElementById(`respuesta`)?.classList.add('error');
-    alert('MAL, prueba otra vez.');
-    vidas--;
-    actualizarVidas();
-}
+    fallar() {
+        document.getElementById(`respuesta`)?.classList.add('error');
+        alert('MAL, prueba otra vez.');
+        this.vidas--;
+        this.actualizarVidas();
+    }
 
-function gameOver() {
-    alert('GAME OVER');
-    (<HTMLInputElement>document.getElementById(`calcularBoton`))!.disabled = true;
-    mostrar('empezar');
-    // TODO añadir resultado correcto
+    gameOver() {
+        alert('GAME OVER');
+        (<HTMLInputElement>document.getElementById(`calcularBoton`))!.disabled = true;
+        this.mostrar('empezar');
+
+        // TODO: Add correct result
+        this.formulario!.innerHTML += `<p>La respuesta correcta es <span class="resultado-number">${this.resultado}</span>.</p>`
+    }
+
+    quitarError() {
+        document.getElementById(`respuesta`)?.classList.remove('error');
+    }
+
+    randomNumber(lessThan) {
+        return Math.floor(Math.random() * lessThan);
+    }
+
+    esconder(paraEsconder) {
+        document.getElementById(paraEsconder)!.style.display = 'none';
+    }
+
+    mostrar(paraMostrar) {
+        document.getElementById(paraMostrar)!.style.display = 'block';
+    }
 };
 
-function quitarError() {
-    document.getElementById(`respuesta`)?.classList.remove('error');
-}
-
-function randomNumber(lessThan) {
-    return Math.floor(Math.random() * lessThan);
-}
-
-function esconder(paraEsconder) {
-    document.getElementById(`${paraEsconder}`)!.style.display = 'none';
-}
-
-function mostrar(paraMostrar) {
-    document.getElementById(`${paraMostrar}`)!.style.display = 'block';
-}
+// Create an instance of the MathGame class
+const homeWork = new Homework();
